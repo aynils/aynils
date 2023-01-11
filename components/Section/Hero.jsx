9763186a938cbba2e824b04f99dc44b9a39bf89b
Seraphin <svandegar@hotmail.com>
2023-01-11 19:18:12 -0400

    DESIGN - Fix radient going to the right

--- components/Section/Hero.jsx
+++ components/Section/Hero.jsx
@@ -5,26 +5,27 @@ import ArrowDownIcon from '/public/static/icons/arrow-down.svg'
 const Hero = () => {
   const { t } = useTranslation('common')
   return (
-    <section className="mb-20 w-screen ">
-      <svg
-        data-name="Layer 1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-        preserveAspectRatio="none"
-        className="svg absolute -z-10 h-[750px] overflow-hidden sm:h-[560px] sm:w-[250%]"
-      >
-        <defs>
-          <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
-            <stop stopColor="#E4FFFF" offset="0%" />
-            <stop stopColor="#BAFAFF" offset="100%" />
-          </linearGradient>
-        </defs>
-        {/*TODO: Fix radient going to the right*/}
-        <path
-          fill="url(#sw-gradient-0)"
-          d="M 0.351 264.418 C 0.351 264.418 33.396 268.165 47.112 270.128 C 265.033 301.319 477.487 325.608 614.827 237.124 C 713.575 173.504 692.613 144.116 805.776 87.876 C 942.649 19.853 1317.845 20.149 1440.003 23.965 C 1466.069 24.779 1440.135 24.024 1440.135 24.024 L 1440 0 L 1360 0 C 1280 0 1120 0 960 0 C 800 0 640 0 480 0 C 320 0 160 0 80 0 L 0 0 L 0.351 264.418 Z"
-        />
-      </svg>
+    <section className="mb-20 ">
+      <div className="absolute -z-10 w-screen overflow-hidden">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          preserveAspectRatio="none"
+          className="svg h-[750px] sm:h-[560px] sm:w-[250%]"
+        >
+          <defs>
+            <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+              <stop stopColor="#E4FFFF" offset="0%" />
+              <stop stopColor="#BAFAFF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <path
+            fill="url(#sw-gradient-0)"
+            d="M 0.351 264.418 C 0.351 264.418 33.396 268.165 47.112 270.128 C 265.033 301.319 477.487 325.608 614.827 237.124 C 713.575 173.504 692.613 144.116 805.776 87.876 C 942.649 19.853 1317.845 20.149 1440.003 23.965 C 1466.069 24.779 1440.135 24.024 1440.135 24.024 L 1440 0 L 1360 0 C 1280 0 1120 0 960 0 C 800 0 640 0 480 0 C 320 0 160 0 80 0 L 0 0 L 0.351 264.418 Z"
+          />
+        </svg>
+      </div>
       <container className="mx-auto block max-w-7xl">
         <div className="px-6 pt-0 text-center text-gray-800 md:py-20 md:px-24  lg:text-left">
           <div className="container mx-auto ">
