ee4898ead1e7b6e999854eac710dec0cd3532c3f
Seraphin <svandegar@hotmail.com>
2023-05-03 13:59:58 -0300

    DESIGN - Sticky navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -7,29 +7,30 @@ import { useTranslation } from 'next-i18next'
 const Navbar = () => {
   const { t } = useTranslation('common')
   return (
-    <nav className="mx-auto flex h-24 w-full max-w-7xl items-center justify-between bg-primary-0 pl-8 pr-8 pt-5 pb-2">
-      <Link href="/" aria-label="home page">
-        <LogoHorizontal className="mb-2 h-32 w-full" />
-      </Link>
-      <div className="flex items-center text-base leading-5">
-        <div className="hidden flex-nowrap lg:flex">
-          {headerNavLinks.map((link) => {
-            if (link.subLinks) {
-              return (
-                <div
-                  key={link.title}
-                  className="dropdown text-primary-900 relative p-1 text-2xl font-light hover:text-primary-600 dark:text-primary-100 dark:hover:text-primary-400 lg:p-4"
-                >
-                  <button
-                    className="dropdown-toggle"
-                    id={`menu-button-${link.title}`}
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
+    <nav className="sticky top-0 z-50  bg-primary-0 ">
+      <div className="mx-auto flex h-20 w-full max-w-7xl items-center justify-between pl-8 pr-8 pt-5 pb-2">
+        <Link href="/" aria-label="home page">
+          <LogoHorizontal className="mb-2 h-32 w-full" />
+        </Link>
+        <div className="flex items-center text-base leading-5">
+          <div className="hidden flex-nowrap lg:flex">
+            {headerNavLinks.map((link) => {
+              if (link.subLinks) {
+                return (
+                  <div
+                    key={link.title}
+                    className="dropdown text-primary-900 relative p-1 text-2xl font-light hover:text-primary-600 dark:text-primary-100 dark:hover:text-primary-400 lg:p-4"
                   >
-                    {t(link.title)}
-                  </button>
-                  <ul
-                    className="
+                    <button
+                      className="dropdown-toggle"
+                      id={`menu-button-${link.title}`}
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      {t(link.title)}
+                    </button>
+                    <ul
+                      className="
                           dropdown-menu
                           absolute
                           !-left-4
@@ -47,13 +48,13 @@ const Navbar = () => {
                           text-base
                           shadow-lg
                         "
-                    aria-labelledby={`menu-button-${link.title}`}
-                  >
-                    {link.subLinks.map((subLink) => (
-                      <Link
-                        key={`${link.href}${subLink.href}`}
-                        href={`${link.href}${subLink.href}`}
-                        className="
+                      aria-labelledby={`menu-button-${link.title}`}
+                    >
+                      {link.subLinks.map((subLink) => (
+                        <Link
+                          key={`${link.href}${subLink.href}`}
+                          href={`${link.href}${subLink.href}`}
+                          className="
                                   dropdown-item
                                   block
                                   w-full
@@ -66,29 +67,30 @@ const Navbar = () => {
                                   text-primary-700
                                   hover:bg-primary-100
                                 "
-                      >
-                        {t(subLink.title)}
-                      </Link>
-                    ))}
-                  </ul>
-                </div>
-              )
-            } else {
-              return (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="dropdown text-primary-900  relative p-1  text-2xl font-light hover:text-primary-600 dark:text-primary-100 dark:hover:text-primary-400 lg:p-4"
-                >
-                  {t(link.title)}
-                </Link>
-              )
-            }
-          })}
-        </div>
+                        >
+                          {t(subLink.title)}
+                        </Link>
+                      ))}
+                    </ul>
+                  </div>
+                )
+              } else {
+                return (
+                  <Link
+                    key={link.title}
+                    href={link.href}
+                    className="dropdown text-primary-900  relative p-1  text-2xl font-light hover:text-primary-600 dark:text-primary-100 dark:hover:text-primary-400 lg:p-4"
+                  >
+                    {t(link.title)}
+                  </Link>
+                )
+              }
+            })}
+          </div>
 
-        {/*<ThemeSwitch />*/}
-        <MobileNavbar />
+          {/*<ThemeSwitch />*/}
+          <MobileNavbar />
+        </div>
       </div>
     </nav>
   )
