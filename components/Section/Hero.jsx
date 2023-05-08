e74f1b50c07b4052df57ac5a04551ab907089bbb
Seraphin <svandegar@hotmail.com>
2023-05-08 19:54:45 -0300

    DESIGN - Restyle hero

--- components/Section/Hero.jsx
+++ components/Section/Hero.jsx
@@ -8,14 +8,16 @@ const Hero = () => {
   return (
     <GradientSection>
       <container className="mx-auto block max-w-7xl">
-        <div className="px-6 pt-0 pb-8 text-center text-gray-800 lg:px-24 lg:pt-20 lg:pb-24  lg:text-left">
+        <div className="px-6 pt-0 pb-8 lg:px-24 lg:py-32">
           <div className="container mx-auto ">
-            <div className="flex grid items-center  gap-12 lg:grid-cols-3">
+            <div className="grid items-center  gap-12 lg:grid-cols-3">
               <div className="col-span-2 mt-12 flex flex-col justify-center lg:mt-0">
-                <h1>
-                  {t('our_mission')} <br />
-                </h1>
-                <p className="mb-8 mt-10 text-2xl text-secondary-800">{t('mission_statement')}</p>
+                {/*<h1>*/}
+                {/*  {t('our_mission')} <br />*/}
+                {/*</h1>*/}
+                <p className="mb-8 mt-10 font-title text-3xl text-secondary-800 lg:mt-0">
+                  {t('mission_statement')}
+                </p>
 
                 <Button
                   color={'secondary'}
